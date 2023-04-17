c0103b07aa51c32c178dab723aeb1c37515e576a
Prathyu8 <naggireddy.prathyusha@gmail.com>
2023-04-17 18:59:25 +0530

    Reddy

---
+++ Array.java/src/demo.java
@@ -0,0 +1,17 @@
+
+public class demo{
+
+		public class ArrDemo {
+			public static void main(String[] args) {
+				int mydata[] = new int[3];}
+			
+				// arrys also auto initialised in java // 1d , int , size = 3 for( int i=0; i<data.length; i++)System.out.println(data[i]);
+			/* * int mydata[]; *
+			 *  mydata = new int [3];
+			 *   mydata = new int [13];
+			 *    * * mydata = new int [30]; //possible */
+			} 
+		} // collection of homogeneous data // collection of int // collection of char
+			
+		
+
